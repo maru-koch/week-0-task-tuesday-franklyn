16c91fd88f260f64be427bae78ef97ee5b16d29a
maru_koch <nwokochafranklyn@gmail.com>
2022-02-01 18:33:25 +0100

    Task accomplished and tested. all good

--- prime_number.py
+++ prime_number.py
@@ -1,21 +1,22 @@
 def is_prime(num):
     """
         - this function estimates a prime Number
+        - in the context of this evaluation, one is considered to be an Invalid input
     """
     prime = [1, num]
     primeFactors = []
 
-    if type(num) == int and num >= 1:
+    if num >= 1:
 
         for index in range(1, num+1):
 
             if num % index == 0:
-                
+
                 primeFactors.append(index)
 
         if prime == primeFactors:
 
-            return "{} is a prime number".format(num)
+            return "\t{} is a prime number".format(num)
 
         else:
             
@@ -23,5 +24,8 @@ def is_prime(num):
     else:
         return "{} is invalid. Please enter a positive integer greater than 1".format(num)
 
-print(is_prime(8.0))
+num = int(input("Enter a number: "))
+
+for x in num:
+    print(is_prime(x))
 
